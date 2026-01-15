1a8b915407879926a4b4f04f7608f6d26265a33f
kai <aeth.kr@gmail.com>
2026-01-15 13:20:47 -0600

    Fix hypotenuse calculation by using math.sqrt
    Fixed import issue by using math.sqrt for hypotenuse calculation.

--- COSC3339_Inclass_One.py
+++ COSC3339_Inclass_One.py
@@ -14,7 +14,7 @@ import math
 
 # This method contains a bug. In your commit note, state the bug and how you fixed it
 def calculate_hypotenuse(side_a, side_b):
-    result = sqrt((side_a ** 2) + (side_b ** 2))  
+    result = math.sqrt((side_a ** 2) + (side_b ** 2))  
     return result
 
 # This method contains a bug. In your commit note, state the bug and how you fixed it
