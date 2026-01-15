7e2c1d52599b2bd935502507bc231828e2957685
k128c <aeth.kr@gmail.com>
2026-01-15 13:31:44 -0600

    Change bottom_half of calculate_shipping_cost
    Change base_cost for International shipments to 20.15
    Change minimum weight to be 15
    Set extra lb cost to packages more than 10lbs instead

--- COSC3339_Inclass_One.py
+++ COSC3339_Inclass_One.py
@@ -39,12 +39,12 @@ def calculate_shipping_cost(weight, destination):
             cost = base_cost + (extra_weight * 1.0)
             
     elif destination == "International":
-        base_cost = 15.0
-        if weight <= 5:
+        base_cost = 20.15
+        if weight <= 15:
             cost = base_cost
         else:
-            # Over 5 lbs, add $5 per extra lb
-            extra_weight = weight - 5
+            # Over 10 lbs, add $5 per extra lb
+            extra_weight = weight - 10
             cost = base_cost + (extra_weight * 5.0)
             
     else:
